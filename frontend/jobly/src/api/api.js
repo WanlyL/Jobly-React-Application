c479986ba88f9b65401a12631e2dc5d171a61755
WanlyL <138504923+WanlyL@users.noreply.github.com>
2024-03-30 21:33:07 -0400

    Created API helper for application

--- frontend/jobly/src/api/api.js
+++ frontend/jobly/src/api/api.js
@@ -2,16 +2,7 @@ import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
-/** API Class.
- *
- * Static class tying together methods used to get/send to to the API.
- * There shouldn't be any frontend-specific stuff here, and there shouldn't
- * be any API-aware stuff elsewhere in the frontend.
- *
- */
-
 class JoblyApi {
-  // the token for interactive with the API will be stored here.
   static token;
 
   static async request(endpoint, data = {}, method = "get") {
@@ -32,37 +23,30 @@ class JoblyApi {
     }
   }
 
-  // Individual API routes
-
-  /** Get the current user. */
 
   static async getCurrentUser(username) {
     let res = await this.request(`users/${username}`);
     return res.user;
   }
 
-  /** Get companies (filtered by name if not undefined) */
 
   static async getCompanies(name) {
     let res = await this.request("companies", { name });
     return res.companies;
   }
 
-  /** Get details on a company by handle. */
 
   static async getCompany(handle) {
     let res = await this.request(`companies/${handle}`);
     return res.company;
   }
 
-  /** Get list of jobs (filtered by title if not undefined) */
 
   static async getJobs(title) {
     let res = await this.request("jobs", { title });
     return res.jobs;
   }
 
-  /** Apply to a job */
 
   static async applyToJob(username, id) {
     await this.request(`users/${username}/jobs/${id}`, {}, "post");
